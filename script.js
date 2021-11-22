5ba08f168fd660e2b2727de07898c882a82e96ed
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2021-11-22 14:53:20 -0300

    Adicionando JS no Cardápio

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-var nome = "Jefferson";
+const btnCardapio = document.querySelectorAll(".lista-cardapio a");
+const modalCardapio = document.querySelectorAll(".js-cardapio");
 
-console.log(nome)
+console.log(btnCardapio)
+
+const activeTab = (index) => {
+    // btnCardapio.prevent.preventDefault();
+
+    modalCardapio.forEach(section => {
+        section.classList.remove("ativo")
+    });
+
+    modalCardapio[index].classList.add("ativo");
+}
+
+btnCardapio.forEach((itemBtn, index) => {
+    itemBtn.addEventListener('click', () => {
+        activeTab(index)
+    });
+})
+
+
+
+
+
+
